50db108cb0f55dfe7bb6ce1f14f53957aeb92252
parksangwoo <b01056721617@gmail.com>
2021-05-15 02:52:10 +0900

    CREATE app.ts

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import "reflect-metadata";
+import path from "path";
+import dotenv from "dotenv";
+import { logger } from "./shared/logger";
+import { initApplication } from "./loader";
+
+dotenv.config({ path:})
