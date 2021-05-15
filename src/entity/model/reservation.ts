cbe029b437314f83dd6b081c71395df6fe1f61f2
parksangwoo <b01056721617@gmail.com>
2021-05-15 21:58:25 +0900

    UPDATE reservation Table

--- src/entity/model/reservation.ts
+++ src/entity/model/reservation.ts
@@ -1,5 +1,7 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne } from "typeorm";
 import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
+import { kidInformation } from "./kid_information";
+import { User } from "./user";
 
 @Entity("reservation_tbl")
 export class Reservation extends EntityWithIdColumn {
@@ -11,4 +13,11 @@ export class Reservation extends EntityWithIdColumn {
 
     @Column({ type: "tinyint", default: false })
     reservation: boolean;
+
+    @JoinColumn({ name: "user_id"})
+    @ManyToOne(() => User, user => user.id)
+    user_id: User;
+
+    @OneToOne(() => kidInformation, kidinformation => kidinformation.reservation_id)
+    kidInformation: kidInformation;
 }
