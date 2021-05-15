f854e23b84bfb73d0cea3cfb579037094b40b446
parksangwoo <b01056721617@gmail.com>
2021-05-15 22:26:48 +0900

    CREATE middleware

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,14 @@
+import { async } from "rxjs";
+import { BusinessLogic } from "../shared/BusinessLogicInterface";
+import { logger } from "../shared/logger";
+
+export const errorHandler = (myFunc: BusinessLogic): BusinessLogic => {
+    return async (req, res, next) => {
+        try {
+            await myFunc(req, res, next);
+        } catch(err) {
+            logger.error(err.message);
+            next(err);
+        }
+    }
+}
---
+++ src/middleware/verifyToken.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import { BusinessLogic } from "../shared/BusinessLogicInterface";
+import { errorHandler } from './errorHandler';
+import { TokenPayload } from './../shared/TokenPayloadInterface';
+import { config } from "../config";
+import { BadRequestError, ExpiredTokenError, UnAuthorizedTokenError } from "../shared/exception";
+
+const verifyTokenLogic: (type: string, headers: string) => BusinessLogic =
+(type: string, headers: string) => (req, res, next) => {
+  try {
+    const token: string = req.headers[headers] as string;
+    if(!token) {
+      return next(new BadRequestError());
+    }
+    const payload: TokenPayload = jwt.verify(token.slice(7), config.jwtSecret) as TokenPayload;
+    if(payload.type !== type) {
+      return next(new UnAuthorizedTokenError());
+    }
+    req.decoded = payload;
+    next();
+  } catch(err) {
+    if(err.message === "TokenExpiredError") {
+      next(new ExpiredTokenError());
+    } else {
+      next(new UnAuthorizedTokenError());
+    }
+  }
+}
+
+const verifyTokenMiddleware: BusinessLogic = errorHandler(verifyTokenLogic("access", "authorization"));
+const verifyRefreshTokenMiddleware: BusinessLogic = errorHandler(verifyTokenLogic("refresh", "refresh-token"));
+export { verifyTokenMiddleware, verifyRefreshTokenMiddleware }
