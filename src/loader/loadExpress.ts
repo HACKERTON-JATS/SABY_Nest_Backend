c038fb3e52e797f7c2d0db3ecf88f6654bf673f4
parksangwoo <b01056721617@gmail.com>
2021-05-15 23:48:25 +0900

    UPDATE loadExpress

--- src/loader/loadExpress.ts
+++ src/loader/loadExpress.ts
@@ -3,3 +3,39 @@ import { NextFunction, Request, Response, Application } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import cors from "cors";
+import { config } from "../config";
+import { HttpError, NotFoundError } from "../shared/exception";
+import { sabyRouter } from "../router";
+import { logger } from "../shared/logger";
+
+export const loadExpress = (app: Application) => {
+    app.set("port", config.ServicePort || "3000");
+
+    app.use(morgan("combined", {
+        stream: fs.createWriteStream("./logs/log.log", { encoding: "utf8" }),
+    }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cors());
+
+    app.use("/", sabyRouter());
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        next(new NotFoundError(req.url));
+    });
+
+    app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+        logger.info(err.message);
+        const statusCode: number = err.statusCode || 500;
+        res.status(statusCode)
+        .json({
+            statusCode: statusCode,
+            message: err.message,
+            timeStamp: new Date(),
+        });
+    });
+
+    app.listen(app.get("port"), () => {
+        console.log("server on", app.get("port"));
+    });
+}
