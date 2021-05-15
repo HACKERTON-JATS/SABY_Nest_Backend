2536e82d8b7832483ea985488bf951fa899ccae0
parksangwoo <b01056721617@gmail.com>
2021-05-15 21:57:33 +0900

    UPDATE admin

--- src/entity/model/admin.ts
+++ src/entity/model/admin.ts
@@ -1,9 +1,12 @@
 import { Entity, OneToMany, PrimaryColumn } from "typeorm";
+import { Chat } from "./chat";
 import { Room } from "./room";
 
 @Entity("admin_tbl")
 export class Admin {
-    @PrimaryColumn({ type: "varchar", length: 20, default: "code123" })
-    @OneToMany(() => Room, room => room.admin_code)
+    @PrimaryColumn({ type: "varchar", length: 20, default: "code123"})
     code: string;
+
+    @OneToMany(() => Room, room => room.admin_code)
+    rooms: Room[];
 }
