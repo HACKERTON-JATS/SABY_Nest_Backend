d34fb8207a0274b3f244ed90949607589aa0ad57
parksangwoo <b01056721617@gmail.com>
2021-05-15 23:49:43 +0900

    CREATE index.ts

---
+++ src/entity/model/index.ts
@@ -0,0 +1,15 @@
+import { Admin } from "./admin";
+import { Chat } from "./chat";
+import { kidInformation } from "./kid_information";
+import { Reservation } from "./reservation";
+import { Room } from "./room";
+import { User } from "./user"; 
+
+export {
+    Admin, 
+    Chat, 
+    kidInformation, 
+    Reservation, 
+    Room, 
+    User
+}
