8a5e2f086f40333d24d1170f93dc1cb3455dbfe2
parksangwoo <b01056721617@gmail.com>
2021-05-17 11:24:28 +0900

    UPDATE kidInformationRepository

--- src/entity/entity-repository/kidInformationRepository.ts
+++ src/entity/entity-repository/kidInformationRepository.ts
@@ -8,8 +8,9 @@ export class KidInformationRepository extends Repository<kidInformation> {
         return getCustomRepository(KidInformationRepository);
     }
 
-    public async getInformation(reservation_id: number): Promise<KidInformationRepository> {
+    public async getInformation(reservation_id: number): Promise<kidInformation> {
         return this.createQueryBuilder()
+            .where("kidInformation.reservation_id = :id", { id: reservation_id})
             .getOne()
     }
 }
