b22a95d138384337a7b0fda4fea58b451b23ecc7
parksangwoo <b01056721617@gmail.com>
2021-05-19 14:47:07 +0900

    UPDATE userService

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -6,17 +6,39 @@ export class UserService {
         private userRepository: UserRepository
     ) { }
 
-    public async createUser(user: User): Promise<void> {
-         
+    public async checkEmail(email: string): Promise<boolean> {
+        const checkEmail: User = await this.userRepository.findOne({
+            where: { email: email }
+        });
+
+        return !checkEmail;   // email이 존재하면 false를 반환
     }
 
-    public async checkOverlap(email: string): Promise<boolean> {
-        const emails: string[] = await this.userRepository.selectEmail();
-        for(let i = 0; i < emails.length; i++) {
-            if(emails[i] === email) {
-                return false;
-            }
-        }
-        return true;
+    public async checkCode(code: string): Promise<boolean> {
+        const checkCode: User = await this.userRepository.findOne({
+            // 인증 코드 확인 구현
+        })
+
+        return !checkCode;
+    }
+
+    public async checkId(id: string): Promise<boolean> {
+        const checkId: User = await this.userRepository.findOne({
+            where: { id: id }
+        })
+
+            return !checkId;   // id가 존재하면 false를 반환
+    }
+
+    public async checkNickname(nickname: string): Promise<boolean> {
+        const checkNickname: User = await this.userRepository.findOne({
+            where: { nickname: nickname }                      
+        })
+
+        return !checkNickname;   // nickname이 존재하면 false를 반환
+    }
+
+    public async createUser(user: User): Promise<void> {
+        await this.userRepository.manager.save(user);
     }
 }
