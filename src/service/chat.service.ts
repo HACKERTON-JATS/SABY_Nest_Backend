ac1e3764b03759c5cd3343682805f4c4dd8bb046
parksangwoo <b01056721617@gmail.com>
2021-05-25 19:20:14 +0900

    UPDATE chatService

--- src/service/chat.service.ts
+++ src/service/chat.service.ts
@@ -1,11 +1,24 @@
 import { ChatRepository } from "../entity/entity-repository/chatRepository";
-import { Chat } from "../entity/model/chat";
-import socketio from "socket.io";
+import { RoomRepository } from "../entity/entity-repository/roomRepository";
+import { UserRepository } from "../entity/entity-repository/userRepository";
+import { Chat } from "../entity/model";
 
 export class ChatService {
     constructor(
-        private chatRepository: ChatRepository
+        private chatRepository: ChatRepository,
+        private roomRepository: RoomRepository,
+        private userRepository: UserRepository
     ) { }
 
-    
+    public async sendMessage(msg: string, user_id: number, room_id: string): Promise<string> {
+        const user = await this.userRepository.findOne(user_id);
+        const room = await this.roomRepository.find({
+            where: { id: room_id }
+        });
+        const newChat = await this.chatRepository.create({
+            message: msg,
+            user_id: user,
+            room_id: room 
+        })
+    }
 }
