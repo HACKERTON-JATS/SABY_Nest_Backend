cd2a583918ede372d0f534393b3cc299e7a64316
parksangwoo <b01056721617@gmail.com>
2021-06-03 20:15:29 +0900

    UPDATE chat Service

--- src/service/chat.service.ts
+++ src/service/chat.service.ts
@@ -1,29 +1,48 @@
 import { ChatRepository } from "../entity/entity-repository/chatRepository";
 import { RoomRepository } from "../entity/entity-repository/roomRepository";
 import { UserRepository } from "../entity/entity-repository/userRepository";
+import { AdminRepository } from "../entity/entity-repository/adminRepository";
+import { BadRequestError } from "../shared/exception";
 
 export class ChatService {
     constructor(
         private chatRepository: ChatRepository,
         private roomRepository: RoomRepository,
-        private userRepository: UserRepository                                                                                                                                                                                                                                                                                                                  
+        private userRepository: UserRepository,
+        private adminRepository: AdminRepository                                                                                                                                                                                                                                                                                                                 
     ) { }
 
-    public async makeRoom(userId: number) {
-        const 
-    }
-    public async sendMessage(msg: string, user_id: number, room_id: string): Promise<string> {
-        const user = await this.userRepository.findOne(user_id);
-        const room = await this.roomRepository.find({
-            where: { id: room_id }
+    public async makeRoom(userId: number): Promise<string> {
+        const existRoom = await this.roomRepository.findOne({
+            where: { user: userId }
         });
-        const newChat = await this.chatRepository.create({
+        if(existRoom) {
+            throw new BadRequestError('더 이상 방을 만들 수 없습니다.');
+        }
+        const user = await this.userRepository.findOne(userId);
+        const admin = await this.adminRepository.findOne();
+        const newRoom = this.roomRepository.create({
+            user: user,
+            admin: admin
+        });
+        await this.roomRepository.save(newRoom);
+        return String(newRoom.id);
+    }
+
+    public async sendMessage(msg: string, userId: number, roomId: string): Promise<string> {
+        const user = await this.userRepository.findOne(userId);
+        const room = await this.roomRepository.findOne(roomId);
+        const newChat = this.chatRepository.create({
             message: msg,
-            user_id: user_id,
-            room_id: room_id
-        })
+            user: user,
+            room: room
+        });
         await this.chatRepository.save(newChat);
 
         return newChat.message;
+    }
+
+    public async getList() {
+        const 
     }
 }
