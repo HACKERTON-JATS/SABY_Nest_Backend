fe2e8f9e41b6eb30600368e8eb39ca04045daff5
parksangwoo <b01056721617@gmail.com>
2021-06-03 09:41:25 +0900

    UPDATE app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,13 @@ import dotenv from "dotenv";
 import { logger } from "./shared/logger";
 import { initApplication } from "./loader";
 
-dotenv.config({ path:})
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
+initApplication()
+    .catch(() => console.error("server start failed"));
+
+process.on("uncaughtException", (err: Error) => {
+    console.error(err);
+    logger.error("uncaughtException");
+    logger.error(err);
+})
