daa5896c943be9be2f7365b686d0dd41e320b197
parksangwoo <b01056721617@gmail.com>
2021-06-04 19:08:34 +0900

    UPDATE chatService

--- src/service/chat.service.ts
+++ src/service/chat.service.ts
@@ -3,20 +3,21 @@ import { RoomRepository } from "../entity/entity-repository/roomRepository";
 import { UserRepository } from "../entity/entity-repository/userRepository";
 import { AdminRepository } from "../entity/entity-repository/adminRepository";
 import { BadRequestError } from "../shared/exception";
+import { Chat, Room } from "src/entity/model";
 
 export class ChatService {
     constructor(
         private chatRepository: ChatRepository,
         private roomRepository: RoomRepository,
         private userRepository: UserRepository,
-        private adminRepository: AdminRepository                                                                                                                                                                                                                                                                                                                 
+        private adminRepository: AdminRepository
     ) { }
 
     public async makeRoom(userId: number): Promise<string> {
         const existRoom = await this.roomRepository.findOne({
             where: { user: userId }
         });
-        if(existRoom) {
+        if (existRoom) {
             throw new BadRequestError('더 이상 방을 만들 수 없습니다.');
         }
         const user = await this.userRepository.findOne(userId);
@@ -42,7 +43,39 @@ export class ChatService {
         return newChat.message;
     }
 
-    public async getList() {
-        const 
+    public async getUserList(): Promise<Room[]> {
+        const rooms = await this.roomRepository.find({});
+        return rooms;
+    }
+
+    public async joinRoom(roomId: number): Promise<Chat[]> {
+        const room = await this.roomRepository.findOne({
+            where: { id: roomId }
+        });
+        if (!room) {
+            throw new BadRequestError('존재하지 않는 방입니다.');
+        }
+        const chats = await this.chatRepository.find({
+            where: { room: roomId },
+            order: {
+                id: "DESC"
+            }
+        });
+
+        return chats;
+    }
+
+    public async deleteRoom(roomId: number): Promise<void> {
+        const room = await this.roomRepository.findOne({
+            where: { id: roomId }
+        });
+        const chat = await this.chatRepository.find({
+            where: { id: roomId }
+        });
+        if(!room) {
+            throw new BadRequestError('존재하지 않는 방입니다.');
+        }
+        await this.roomRepository.remove(room);
+        await this.chatRepository.remove(chat);
     }
 }
