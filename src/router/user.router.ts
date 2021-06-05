27fcd345a364996aec6658b386b653a2396599a3
parksangwoo <b01056721617@gmail.com>
2021-06-05 22:00:44 +0900

    UPDATE userRouter

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { errorHandler } from "../middleware/errorHandler";
 import { UserController } from "../controller/user.controller";
 import { verifyTokenMiddleware } from "../middleware/verifyToken";
+import { validationRequest } from "../middleware/validationRequest";
+import { UserEamilSchema, UserIdSchema, UserNicknameSchema, UserVerifyCodeSchema, UserSignupSchema, UserLoginSchema } from "../shared/DataTransferObject";
 
 const router: Router = Router();
 export const userServiceRouter = (app: Router) => {
@@ -11,36 +13,43 @@ export const userServiceRouter = (app: Router) => {
 
     router.post(
         "/signup",
+        validationRequest(UserSignupSchema),
         errorHandler(userController.createUser)
     );
 
     router.get(
         "/members/email",
+        validationRequest(UserEamilSchema),
         errorHandler(userController.overlapEmail)
     );
 
     router.get(
         "/members/id",
+        validationRequest(UserIdSchema),
         errorHandler(userController.overlapId)
     );
 
     router.get(
         "/members/nickname",
+        validationRequest(UserNicknameSchema),
         errorHandler(userController.overlapNickname)
     );
 
     router.post(
         "/sms-certification/sends",
+        validationRequest(UserEamilSchema),
         errorHandler(userController.sendCode)
     );
 
     router.get(
         "/sms-certification/confirms",
+        validationRequest(UserVerifyCodeSchema),
         errorHandler(userController.checkVerifyCode)
     );
 
     router.post(
         "/login",
+        validationRequest(UserLoginSchema),
         errorHandler(userController.login)
     );
 
