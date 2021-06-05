e1ca3f02a68ae63695e4007cdc983c4ea6ad77bc
parksangwoo <b01056721617@gmail.com>
2021-06-06 00:43:23 +0900

    UPDATE reservationService

--- src/service/reservation.service.ts
+++ src/service/reservation.service.ts
@@ -1,15 +1,33 @@
-import { KidInformationRepository } from "src/entity/entity-repository/kidInformationRepository";
-import { ReservationRepository } from "src/entity/entity-repository/reservationRepository";
+import { KidInformationRepository } from "../entity/entity-repository/kidInformationRepository";
+import { ReservationRepository } from "../entity/entity-repository/reservationRepository";
+import { UserRepository } from "../entity/entity-repository/userRepository";
 import { kidInformation, Reservation } from "src/entity/model";
+import { BadRequestError } from "../shared/exception";
 
 export class ReservationService {
     constructor(
         private reservationRepository: ReservationRepository,
-        private kidInformationRepository: KidInformationRepository
+        private kidInformationRepository: KidInformationRepository,
+        private userRepository: UserRepository
     ) {}
 
-    public async makeReservation(kidInformation: kidInformation, reservation: Reservation): Promise<void> {
-        await this.kidInformationRepository.createInformation(kidInformation);
-        await this.reservationRepository.makeReservation(reservation);
+    public async makeReservation(kidInformation: kidInformation, reservation: Reservation, user_id: number): Promise<void> {
+        const existReservation = await this.reservationRepository.findOne({
+            where: { time: reservation.time }
+        });
+        if(existReservation) {
+            throw new BadRequestError("이미 예약된 시간입니다.");
+        }
+        const user = await this.userRepository.findOne({
+            where: { id: user_id }
+        });
+        await this.reservationRepository.makeReservation(reservation, user);
+        const reservationId = await this.reservationRepository.getReservationId(reservation.time);
+        await this.kidInformationRepository.createInformation(reservationId, kidInformation);
+    }
+
+    public async getReservation(user_id: number): Promise<Date[]> {
+        const time = await this.reservationRepository.getReservationTime(user_id);
+        return time;
     }
 }   
