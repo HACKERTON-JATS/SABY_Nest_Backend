1bf2786a261a7bdd0deb6b3a9cc030af7a14c77e
parksangwoo <b01056721617@gmail.com>
2021-06-06 00:42:08 +0900

    UPDATE reservation Controller

--- src/controller/reservation.controller.ts
+++ src/controller/reservation.controller.ts
@@ -2,17 +2,24 @@ import { ReservationRepository } from "../entity/entity-repository/reservationRe
 import { KidInformationRepository } from "../entity/entity-repository/kidInformationRepository";
 import { BusinessLogic } from "../shared/BusinessLogicInterface";
 import { ReservationService } from "../service/reservation.service";
+import { UserRepository } from "../entity/entity-repository/userRepository";
 
 export class ReservationController {
     private reservationService: ReservationService = new ReservationService(
         ReservationRepository.getQueryRepository(),
-        KidInformationRepository.getQueryRepository()
+        KidInformationRepository.getQueryRepository(),
+        UserRepository.getQueryRepository()
     );
 
     public makeReservation: BusinessLogic = async (req, res, next) => {
-        await this.reservationService.makeReservation(req.body.kidInformation, req.body.reservation);
+        await this.reservationService.makeReservation(req.body.kidInformation, req.body.reservation, req.decoded.sub);
         res.status(200).json({
             message: "success"
         })
+    }
+
+    public getReservation: BusinessLogic = async (req, res, next) => {
+        const time = await this.reservationService.getReservation(req.decoded.sub);
+        res.status(200).json(time);
     }
 }
